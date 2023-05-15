b09acd1bcea2d6c1841250f67a427fcfc57719db
Ko-Htet-Naing <tonybright008@gmail.com>
2023-05-14 23:06:15 -0700

    Build Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Grid, GridItem, Hide, Show, useBreakpointValue } from '@chakra-ui/react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <Grid templateAreas={{
+        base: ` "nav" "main"`,
+        lg: `"nav nav" "aside main"`
+      }} color='blackAlpha.700'>
+        <GridItem area={'nav'} bg='orange.300'>Nav</GridItem>
+        {/* {isLargeScreen &&
+        } */}
+        <Show above='lg'>
+          <GridItem area={'aside'} bg='pink.300'>Aside</GridItem>
+        </Show>
+
+        <GridItem area={'main'} bg='green.300'>Main</GridItem>
+      </Grid>
+    </>
   )
+
 }
 
 export default App
